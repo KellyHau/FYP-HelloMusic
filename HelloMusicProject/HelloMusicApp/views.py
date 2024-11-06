999e5765db311a4dd966f46a6b9ba315f0ae2913
wengyoke <leewengyoke-wm21@student.tarc.edu.my>
2024-11-06 21:27:29 +0800

    connect HelloMusic to firebase
    Able to get data from firebase

--- HelloMusicProject/HelloMusicApp/views.py
+++ HelloMusicProject/HelloMusicApp/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyAFdTQIkXeJCkE760kQH1kZtCjkZnm3o7Q",
+    "authDomain": "hellomusic-63ee9.firebaseapp.com",
+    "databaseURL": "https://hellomusic-63ee9-default-rtdb.firebaseio.com",
+    "projectId": "hellomusic-63ee9",
+    "storageBucket": "hellomusic-63ee9.firebasestorage.app",
+    "messagingSenderId": "186714219968",
+    "appId": "1:186714219968:web:456d77546cc2cc3b73d425",
+}
+
+firebase = pyrebase .initialize_app(config)
+authe = firebase.auth()
+database = firebase.database()
 
 def home(request):
-    context={}
+    music_sheets = database.child('MusicSheet').get().each()
+    
+   # Filter out any empty items
+    valid_music_sheets = [sheet for sheet in music_sheets if sheet.val()]
+    context = {
+        "music_sheets": valid_music_sheets
+    }
     return render(request,"HelloMusicApp/index.html",context)
 
 
